0405cd68fd966616a8a1e1b1b6ec9999752e9537
kk10 <kamy.zero.10@gmail.com>
2025-11-22 22:54:01 +0300

    [BACKEND-38] fix(apps): исправлен префикс роутера с /hint на /checklists

--- src/apps/checklists/api/v1/endpoints/checklists.py
+++ src/apps/checklists/api/v1/endpoints/checklists.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from starlette import status
 
-router = APIRouter(prefix="/hint")
+router = APIRouter(prefix="/checklists")
 
 
 @router.get(path="/ping", summary="Ping", status_code=status.HTTP_200_OK)
