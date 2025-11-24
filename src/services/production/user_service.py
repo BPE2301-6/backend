df82761df0dce60895164e7f2c9d475dac4beb95
kk10 <kamy.zero.10@gmail.com>
2025-11-24 23:39:48 +0300

    [BACKEND-38] feat(apps): реализованы эндпоинты модуля users

--- src/services/production/user_service.py
+++ src/services/production/user_service.py
@@ -28,3 +28,6 @@ class UserServiceImpl(BaseService[...]):
     async def get_all(self) -> list[...]:
         items = await self.store.user_repo().get_all()
         return items
+    
+    async def get_list(self, search: str, limit: int, offset: int) -> tuple[list[dict], int]:
+        return [], 0
