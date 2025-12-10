76b08f94ed628ba6b809953e0250bc29147b8828
kk10 <kamy.zero.10@gmail.com>
2025-12-10 22:48:33 +0300

    [BACKEND-39] feat(apps): реализована валидация эндпоинтов модуля tags

--- src/apps/tags/api/v1/endpoints/tags.py
+++ src/apps/tags/api/v1/endpoints/tags.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, status
 
 from src.core.dependencies import get_service_manager
 from src.services import Service
+from src.schemas.pydantic import TagUpdateRequest, TagResponse
 
 router = APIRouter(prefix="/tags")
 
@@ -12,13 +13,16 @@ router = APIRouter(prefix="/tags")
     "/{tag_id}",
     summary="Обновить тег",
     description=(
-        "Обновляет данные тега по указанному идентификатору. " "Возвращает обновлённый объект."
+        "Обновляет данные тега по указанному идентификатору. "
+        "Возвращает обновлённый объект."
     ),
 )
 async def update_tag(
-    tag_id: UUID, data: dict, service_manager: Service = Depends(get_service_manager)
-):
-    return await service_manager.tag_service().update(tag_id, data)
+    tag_id: UUID,
+    data: TagUpdateRequest,
+    service_manager: Service = Depends(get_service_manager),
+) -> TagResponse:
+    return await service_manager.tag_service().update(tag_id, data.model_dump(exclude_unset=True))
 
 
 @router.delete(
@@ -30,5 +34,8 @@ async def update_tag(
         "При успешном выполнении возвращает статус 204."
     ),
 )
-async def delete_tag(tag_id: UUID, service_manager: Service = Depends(get_service_manager)):
+async def delete_tag(
+    tag_id: UUID,
+    service_manager: Service = Depends(get_service_manager),
+):
     await service_manager.tag_service().delete(tag_id)
