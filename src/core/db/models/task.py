4730ce046a4bcb59f3f18176298e868c34d79ea1
kk10 <kamy.zero.10@gmail.com>
2025-12-23 14:55:19 +0300

    [BACKEND-63] feat(models): добавлено автоматическое обновление поля updated_at

--- src/core/db/models/task.py
+++ src/core/db/models/task.py
@@ -55,7 +55,7 @@ class Task(Base):
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), nullable=False, server_default=func.now()
+        DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now()
     )
 
     project: Mapped[Project] = relationship(back_populates="tasks")
--- src/core/db/models/user.py
+++ src/core/db/models/user.py
@@ -30,7 +30,7 @@ class User(Base):
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), nullable=False, server_default=func.now()
+        DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now()
     )
 
     led_projects: Mapped[list[Project]] = relationship(back_populates="lead")
